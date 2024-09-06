a7a2697f6546bbaf3b2e763424107512b3406d29
ln-dev7 <leonelngoya@gmail.com>
2024-09-06 17:21:50 +0100

    update layout

--- src/App.tsx
+++ src/App.tsx
@@ -52,79 +52,74 @@ export default function App() {
 
   return (
     <main className="relative w-full min-h-screen flex items-start md:items-center justify-center px-4 py-10">
-      <div
-        className="relative w-full h-screen overflow-hidden flex items-center justify-center"
-        id="app"
-      >
-        <div className="flex flex-col items-center w-full z-10">
-          <div className="w-full border-b border-dashed border-slate-200"></div>
-          <div className="w-full flex flex-col gap-8 justify-center items-center h-full max-w-lg mx-auto p-16 bg-white">
-            <div className="relative flex flex-col items-start justify-center w-full">
-              <span className="text-sm font-semibold">→ {password}</span>
-              <motion.div
-                className="mb-3 mt-1 w-full border-2 py-2 px-2 rounded-xl bg-white  h-[52px]"
-                animate={{
-                  ...bounceAnimation,
-                  ...matchAnimation,
-                  ...borderAnimation,
-                }}
-              >
-                <div className="overflow-hidden rounded-lg relative w-fit h-full">
-                  <div className="py-1 px-0 h-full bg-transparent flex items-center justify-center tracking-[0.15em] z-10">
-                    {password.split("").map((_, index) => (
-                      <div
-                        key={index}
-                        className="w-4 shrink-0 h-full flex items-center justify-center"
-                      >
-                        <span className="size-[5px] bg-black rounded-full"></span>
-                      </div>
-                    ))}
-                  </div>
-                  <div className="absolute h-full left-0 w-full top-0 bottom-0 z-0 flex items-center justify-start">
-                    {password.split("").map((letter, index) => (
-                      <motion.div
-                        key={index}
-                        className={`w-4 absolute h-full transition-all duration-300 ease ${getLetterStatus(
-                          letter,
-                          index
-                        )}`}
-                        style={{
-                          left: `${index * 16}px`,
-                          scaleX: confirmPassword[index] ? 1 : 0,
-                          transformOrigin: "left",
-                        }}
-                      ></motion.div>
-                    ))}
-                  </div>
+      <div className="flex flex-col items-center w-full z-10">
+        <div className="w-full border-b border-dashed border-slate-200"></div>
+        <div className="w-full flex flex-col gap-8 justify-center items-center h-full max-w-lg mx-auto p-16 bg-white">
+          <div className="relative flex flex-col items-start justify-center w-full">
+            <span className="text-sm font-semibold">→ {password}</span>
+            <motion.div
+              className="mb-3 mt-1 w-full border-2 py-2 px-2 rounded-xl bg-white  h-[52px]"
+              animate={{
+                ...bounceAnimation,
+                ...matchAnimation,
+                ...borderAnimation,
+              }}
+            >
+              <div className="overflow-hidden rounded-lg relative w-fit h-full">
+                <div className="py-1 px-0 h-full bg-transparent flex items-center justify-center tracking-[0.15em] z-10">
+                  {password.split("").map((_, index) => (
+                    <div
+                      key={index}
+                      className="w-4 shrink-0 h-full flex items-center justify-center"
+                    >
+                      <span className="size-[5px] bg-black rounded-full"></span>
+                    </div>
+                  ))}
                 </div>
-              </motion.div>
+                <div className="absolute h-full left-0 w-full top-0 bottom-0 z-0 flex items-center justify-start">
+                  {password.split("").map((letter, index) => (
+                    <motion.div
+                      key={index}
+                      className={`w-4 absolute h-full transition-all duration-300 ease ${getLetterStatus(
+                        letter,
+                        index
+                      )}`}
+                      style={{
+                        left: `${index * 16}px`,
+                        scaleX: confirmPassword[index] ? 1 : 0,
+                        transformOrigin: "left",
+                      }}
+                    ></motion.div>
+                  ))}
+                </div>
+              </div>
+            </motion.div>
 
-              <motion.div
-                className="w-full overflow-hidden rounded-xl h-[52px]"
-                animate={matchAnimation}
-              >
-                <motion.input
-                  className="w-full h-full border-2 rounded-xl py-3 px-3.5 bg-white outline-none focus:border-slate-900 tracking-[0.4em] placeholder:tracking-normal"
-                  type="password"
-                  placeholder="Confirm Password"
-                  value={confirmPassword}
-                  onChange={handleConfirmPasswordChange}
-                  animate={borderAnimation}
-                />
-              </motion.div>
-            </div>
+            <motion.div
+              className="w-full overflow-hidden rounded-xl h-[52px]"
+              animate={matchAnimation}
+            >
+              <motion.input
+                className="w-full h-full border-2 rounded-xl py-3 px-3.5 bg-white outline-none focus:border-slate-900 tracking-[0.4em] placeholder:tracking-normal"
+                type="password"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+                animate={borderAnimation}
+              />
+            </motion.div>
           </div>
-          <div className="w-full border-t border-dashed border-slate-200"></div>
         </div>
-        <div className="absolute w-full h-full inset-0">
-          <div className="absolute w-full h-full inset-0 px-4 py-0 pointer-events-none">
-            <div className="flex justify-between items-center h-full max-w-[1080px] mx-auto">
-              <div className="bg-slate-200 w-[1px] h-full"></div>
-              <div className="border-r border-slate-200 border-dashed h-full"></div>
-              <div className="border-r border-slate-200 border-dashed h-full"></div>
-              <div className="border-r border-slate-200 border-dashed h-full"></div>
-              <div className="bg-slate-200 w-[1px] h-full"></div>
-            </div>
+        <div className="w-full border-t border-dashed border-slate-200"></div>
+      </div>
+      <div className="absolute w-full h-full inset-0">
+        <div className="absolute w-full h-full inset-0 px-4 py-0 pointer-events-none">
+          <div className="flex justify-between items-center h-full max-w-[1080px] mx-auto">
+            <div className="bg-slate-200 w-[1px] h-full"></div>
+            <div className="border-r border-slate-200 border-dashed h-full"></div>
+            <div className="border-r border-slate-200 border-dashed h-full"></div>
+            <div className="border-r border-slate-200 border-dashed h-full"></div>
+            <div className="bg-slate-200 w-[1px] h-full"></div>
           </div>
         </div>
       </div>
